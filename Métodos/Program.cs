7094c87de0740f26ae8a44abbcdd33dc4eeee7ff
Jander <jander.gneves@senacsp.edu.br>
2025-01-30 20:55:29 -0300

    Métodos

---
+++ Métodos/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao("Jander");
+            Soma(10, 20);
+        }
+        static void Saudacao(string nome)
+        {
+            Console.WriteLine($"Olá {nome}, seja bem-vindo!");
+        }
+
+        static void Soma(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+            Console.WriteLine("A soma de {0} + {1} é igual a {2}. ", a, b, resultado);
+            Console.WriteLine("A soma de " + a + " + " + b + " é igual a " + resultado + ".");
+        }
+    }
+}
