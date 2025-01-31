339b7c4f5e7cef44be41c9cacae60674d36f8a25
Jander <jander.gneves@senacsp.edu.br>
2025-01-30 21:49:20 -0300

    Escrever

---
+++ Area do triangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Area_do_triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+     using System;
+
+    {
+        static void Main()
+        {
+            
+            Console.Write("Digite o valor da base do triângulo: ");
+            double baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.Write("Digite o valor da altura do triângulo: ");
+            double alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            double area = (baseTriangulo * alturaTriangulo) / 2;
+
+            
+            Console.WriteLine($"A área do triângulo é: {area}");
+        }
+    }
---
+++ Escrever/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Escrever
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('*', tam);
+            string textoCentralizado = texto.PadLeft(tam - 2).PadRight(tam - 2);    
+            Console.WriteLine(linha);
+            Console.WriteLine(textoCentralizado);
+            Console.WriteLine(linha);
+        }
+
+         static void Main(string[] args)
+        {
+            Escrever("Jander Guimarães");
+            Console.WriteLine();
+            Escrever("Curso C#");
+            Console.WriteLine();
+            Escrever("Olá");
+            Console.WriteLine();
+        }
+    }
+}
